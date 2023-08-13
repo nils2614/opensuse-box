1c49dcac9b20f47fa9a39b4d4294e5aeedf9de03
Nils Offermann <63887691+nils2614@users.noreply.github.com>
2023-08-13 18:58:51 +0200

    Update host-runner.sh

--- host-runner.sh
+++ host-runner.sh
@@ -6,4 +6,4 @@ parameters="${@}"
 myname="$(basename "$0")"
 executable="${myname%-host}" # Ignore -host suffix in application names
 
-exec flatpak-spawn --host --watch-bus --forward-fd=1 --forward-fd=2 --directory="$(pwd)" --env=TERM=xterm-256color $executable $parameters
+exec flatpak-spawn --host --watch-bus --directory="$(pwd)" --env=TERM=xterm-256color $executable $parameters
